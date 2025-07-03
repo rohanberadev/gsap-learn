e1741e60a71c3daf535e2c5497b90fc75fd81151
Rohan Bera <rohanbera4002@gmail.com>
2025-07-03 11:31:36 +0530

    implemented art

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import gsap from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
 import About from "~/components/About";
+import Art from "~/components/Art";
 import Cocktails from "~/components/Cocktails";
 import Hero from "~/components/Hero";
 import Navbar from "~/components/Navbar";
@@ -14,6 +15,7 @@ export default function Home() {
             <Hero />
             <Cocktails />
             <About />
+            <Art />
         </main>
     );
 }
